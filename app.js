4deead71a0f04eaaad5685c794abb623d8f792c9
Terence Kungade <watipatsa.kungade@outlook.com>
2024-01-28 22:56:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// JAVASCRIPT FOR TOGGLE MENU
+var navLinks =document.getElementById('navLinks');
+
+        function showMenu() {
+            navLinks.style.right = "0";
+        }
+        function hideMenu() {
+            navLinks.style.right = "-200px";
+}
+
+// SLIDESHOW FUNCTION 
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
